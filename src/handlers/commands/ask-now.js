a9df8397c05bacd8fec6515b2e1f17b471126042
siuming <zephyrwong.private@gmail.com>
2017-08-09 16:54:38 +0800

    Add more description when survey is sent out

--- src/handlers/commands/ask-now.js
+++ src/handlers/commands/ask-now.js
@@ -2,7 +2,7 @@ const Survey = require('../../models/survey.js')
 
 function askNow () {
   Survey.send()
-  return 'OK'
+  return 'OK. Survey will be closed after 20 minutes and report will be available afterwards.'
 }
 
 module.exports = askNow
