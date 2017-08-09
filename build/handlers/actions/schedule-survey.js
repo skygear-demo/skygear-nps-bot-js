4fe02b26774af407c8b1fdd86bc113fc345778d1
siuming <zephyrwong.private@gmail.com>
2017-08-09 17:04:22 +0800

    Restrict 1 survey at a time

--- build/handlers/actions/schedule-survey.js
+++ build/handlers/actions/schedule-survey.js
@@ -4,7 +4,7 @@ const Survey = require('../../models/survey.js');
 
 function scheduleSurvey(frequency) {
   Survey.schedule(frequency);
-  return `OK. Next survey is scheduled at ${global.scheduled.nextDate().format('Do MMM YYYY, HH:mm:ss')}.`;
+  return `OK. Next survey is scheduled at ${global.scheduled.nextDate().format('Do MMM YYYY, HH:mm:ss')}. Survey will be closed after 20 minutes and report will be available afterwards.`;
 }
 
 module.exports = scheduleSurvey;
--- src/handlers/commands/ask-now.js
+++ src/handlers/commands/ask-now.js
@@ -1,8 +1,13 @@
 const Survey = require('../../models/survey.js')
 
-function askNow () {
-  Survey.send()
-  return 'OK. Survey will be closed after 20 minutes and report will be available afterwards.'
+async function askNow () {
+  let survey = await Survey.waitingReply
+  if (survey) {
+    return 'Sorry, only 1 survey is allowed at a time.'
+  } else {
+    Survey.send()
+    return 'OK. Survey will be closed after 20 minutes and report will be available afterwards.'
+  }
 }
 
 module.exports = askNow
