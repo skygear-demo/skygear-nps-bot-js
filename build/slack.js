9948f2e4597a04de2ac76032dc60eff317cd1999
SiuMing <zephyrwong.private@gmail.com>
2017-09-23 03:06:06 +0800

    Clean up

--- build/slack.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const WebClient = require('@slack/client').WebClient;
-const SLACK_BOT_TOKEN = require('./config.js').SLACK_BOT_TOKEN;
-
-module.exports = new WebClient(SLACK_BOT_TOKEN);
--- src/slack.js
+++
@@ -1,4 +0,0 @@
-const WebClient = require('@slack/client').WebClient
-const SLACK_BOT_TOKEN = require('./config.js').SLACK_BOT_TOKEN
-
-module.exports = new WebClient(SLACK_BOT_TOKEN)
