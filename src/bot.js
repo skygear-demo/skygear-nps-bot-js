63e5cf16407498da4fc95f82a828ec87cdc1f1f9
SiuMing <zephyrwong.private@gmail.com>
2017-11-10 14:19:24 +0800

    Exclude guest users for the survey

--- src/bot.js
+++ src/bot.js
@@ -13,8 +13,8 @@ module.exports = class Bot {
   // API wrappers
   async fetchUsers () {
     const res = await this._client.users.list()
-    // neither a bot or a former member
-    return res.members.filter(member => !(member.is_bot || member.name === 'slackbot' || member.deleted))
+    // neither a bot or a former member or a guest user
+    return res.members.filter(member => !(member.is_bot || member.name === 'slackbot' || member.deleted || member.is_restricted))
   }
 
   async fetchUser (id) {
